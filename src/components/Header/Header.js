2e0c856a9c28012406a51bc7620fda6f394e3a5d
JorMee <folajor.mee@gmail.com>
2019-11-21 18:15:11 +0100

    FIX position of search box

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,10 @@
 import React from 'react';
 
-import Searchbox from './Searchbox';
-
 const Header = () => {
   return(
     <div className='header'>
       <nav className='header-nav'>
-
         <a href='/' className='brand-logo'><h3>MovieX</h3></a>
-
-        <Searchbox />
       </nav>
     </div>
   )
--- src/components/Viewport/Viewport.js
+++ src/components/Viewport/Viewport.js
@@ -3,6 +3,7 @@ import React, { useContext } from 'react';
 import { MovieContext } from '../../contexts/MovieContext';
 import MovieCard from './MovieCard';
 import MovieDetails from './MovieDetails';
+import Searchbox from './Searchbox';
 
 const Viewport = () => {
   const { movies, movieDetails, getMovieDetails } = useContext(MovieContext);
@@ -13,22 +14,27 @@ const Viewport = () => {
         <div className="result">
           {movieDetails.Title && <MovieDetails />}
         </div>
-        <div className='container grid-container'>
-          {
-            movies.length
-            ? movies.map(movie => {
-              return(
-                <div className="movie" key={movie.imdbID}>
-                  <MovieCard 
-                    key={`front-${movie.imdbID}`} 
-                    movie={movie}
-                    getMovieDetails={getMovieDetails}
-                  />
-                </div>
-              )
-            }) 
-            : <h3>Welcome, Enter the title of the movie you would like to review in the search box.</h3>
-          }
+        <div className='container'>
+          <div className="search">
+            <Searchbox />
+          </div>
+          <div className="grid-container">
+            {
+              movies.length
+              ? movies.map(movie => {
+                return(
+                  <div className="movie" key={movie.imdbID}>
+                    <MovieCard 
+                      key={`front-${movie.imdbID}`} 
+                      movie={movie}
+                      getMovieDetails={getMovieDetails}
+                    />
+                  </div>
+                )
+              }) 
+              : <h3>Welcome to MovieX, Enter the title of the movie you would like to review in the search box.</h3>
+            }
+          </div>
         </div>
       </div>
     </div>
